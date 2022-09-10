a3b7863af95a6321717f42fe55612eaa207d8dc1
JavierBonilla10 <113258881+JavierBonilla10@users.noreply.github.com>
2022-09-10 12:34:02 -0600

    Ejercicio 10

---
+++ HT3_Ejer10/src/main/java/com/mycompany/ht3_ejer10/HT3_Ejer10.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ht3_ejer10;
+
+/**
+ *
+ * @author Javier
+ */
+public class HT3_Ejer10 {
+
+    public static void main(String[] args) {
+        int numero = 1;
+        while (numero <= 100) {
+            System.out.print("numero -> " + numero + "\n");
+            numero++;
+        }
+    }
+}
